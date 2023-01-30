dae1f18a3e033eabc59ab68f5ecfc1873828aff1
Victor Marques Ribeiro <victormarquess11@gmail.com>
2023-01-30 00:57:50 -0300

    [ADD] send order to whatsapp

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,10 @@
-let pratoSelecionado = false;
-let bebidaSelecionada = false;
-let sobremesaSelecionada = false;
+let selectedPrato = null;
+let selectedBebida = null;
+let selectedSobremesa = null;
+let pricePrato = 0;
+let priceBebida = 0;
+let priceSobremesa = 0;
+let totalPrice = 0;
 
 function selectItem(item, section) {
   toggleAlreadySelected(item, section);
@@ -8,11 +12,15 @@ function selectItem(item, section) {
   if (item !== null) {
     item.classList.toggle('selecionado');
   }
-
   toggleCheckmark(item);
 
-  registerSelectedSection(section);
+  title = checkTitle(item);
+  price = checkPrice(item);
+
+  registerSelectedSection(title, section, price);
+  computePrice();
   checkAllSelected();
+  changeWppLink();
 
 }
 
@@ -35,7 +43,7 @@ function toggleAlreadySelected(item, section) {
 }
 
 function checkAllSelected() {
-  if (!!pratoSelecionado && !!bebidaSelecionada && !!sobremesaSelecionada) {
+  if (!!selectedPrato && !!selectedBebida && !!selectedSobremesa) {
     const botaoFecharPedido = document.querySelector(".btn-fechar-pedido");
     botaoFecharPedido.disabled = false;
     botaoFecharPedido.innerText = "Fechar pedido";
@@ -44,14 +52,48 @@ function checkAllSelected() {
   }
 }
 
-function registerSelectedSection(section) {
+function registerSelectedSection(title, section, price) {
   if (section === 'prato') {
-    pratoSelecionado = true;
+    selectedPrato = title;
+    pricePrato = parseFloat(price.replace(',', '.').replace("R$ ", ""));
   }
   if (section === 'bebida') {
-    bebidaSelecionada = true;
+    selectedBebida = title;
+    priceBebida = parseFloat(price.replace(',', '.').replace("R$ ", ""));
   }
   if (section === 'sobremesa') {
-    sobremesaSelecionada = true;
+    selectedSobremesa = title;
+    priceSobremesa = parseFloat(price.replace(',', '.').replace("R$ ", ""));
   }
+}
+
+function changeWppLink() {
+  const elementLink = document.querySelector('.wpp-link');
+  orderString = `Olá, gostaria de fazer o pedido:
+  - Prato: ${selectedPrato} - R$ ${pricePrato.toFixed(2)}
+  - Bebida: ${selectedBebida} - R$ ${priceBebida.toFixed(2)}
+  - Sobremesa: ${selectedSobremesa} - R$ ${priceSobremesa.toFixed(2)}
+  Total: R$ ${totalPrice}`
+
+  elementLink.href = "https://wa.me/5521999999999?text=" + encodeURIComponent(orderString);
+}
+
+function computePrice() {
+  totalPrice = (pricePrato + priceBebida + priceSobremesa).toFixed(2);
+}
+
+function checkTitle(item) {
+  const elementTitle = item.querySelector('.titulo');
+  if (elementTitle !== null) {
+    return elementTitle.innerText;
+  }
+  return null;
+}
+
+function checkPrice(item) {
+  const elementPrice = item.querySelector('.preco');
+  if (elementPrice !== null) {
+    return elementPrice.innerText;
+  }
+  return null;
 }
